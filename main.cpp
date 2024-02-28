a524d34d1e6525fe12bcff8f0fdec8efb09f4765
duong <duongew123@gmail.com>
2024-02-28 22:10:53 +0700

    bt

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cin >> n;
+    string x = "*";
+    string o = " ";
+//    string a[n][n];
+//    int trong = 0;
+//    for (int i = 0; i < n; ++i)
+//    {
+//        for (int y = 0; y < n; ++y)
+//        {
+//            if (y < trong)a[i][y] = o;
+//            else a[i][y] = x;
+//        }
+//        ++trong;
+//    }
+//    
+//    
+//    for (int i = 0; i < n; ++i)
+//    {
+//        for (int y = 0; y < n; ++y)
+//        {
+//            cout << a[i][y];
+//        }
+//        cout << endl;
+//    }
+    
+    
+    
+    
+    
+//    ##tam giac 2a
+    
+//    int dem = n;
+//    int dem_trong = n - 1;
+//    int dem_sao = 0;
+//    for (int i = 0; i < n; ++i)
+//    {
+//        for (int y = 0; y < dem; ++y)
+//        {
+//            if (y < dem_trong) {cout << o;}
+//            else cout << x;
+//        }
+//        ++dem;
+//        --dem_trong;
+//        cout << endl;
+//    }
+    
+    
+    
+    
+//    ##tam giac 2b
+    
+//    int dem = n;
+//    int dem_trong = 0;
+//    int so_sao = 1 + 2 * (n - 1);
+//    for (int i = 0; i < n; ++i)
+//    {
+//        for (int y = 0; y < so_sao; ++y)
+//        {
+//            if (y < dem_trong) {cout << o;}
+//            else cout << x;
+//        }
+//        --so_sao;
+//        ++dem_trong;
+//        cout << endl;
+//    }
+//}
